0e4a8bdd42d548284cf19ba26460c51eedc290a5
Mashhurbek <104623162+Mashhurbe@users.noreply.github.com>
2022-05-20 13:14:47 +0500

    Add files via upload

---
+++ Django1/website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'base.html')
+
+def home(request):
+    return render(request, 'navbar.html')
+def home(request):
+    return render(request, 'footer.html')
+def home(request):
+    return render(request, 'home.html')
